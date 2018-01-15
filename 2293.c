df28b8b195610c333950821eb104be779601aa81
KimDongUn <dong7061@gmail.com>
2018-01-15 21:49:10 +0900

    동전1

--- 2293.c
+++ 2293.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
-#define MAXN 50000
+#define MAXN 100
+#define MAXM 10000
+
+int memo[MAXN][MAXM];
+long long  dp[10001];
 
 int coin(int money,int n,int bills[])
 {
-	static int  memo[MAXN];
-
-	int t = 0, cnt = 0;;
-
-	if (n==1)
+	int t = 0, cnt = 0;
+	if (memo[n][money] != 0)
+	{
+		return memo[n][money];
+	}
+	if (n==1 || money ==0 )
 	{
 		if (money % bills[0] == 0)
 		{
-			return memo[money] = 1;
+			return 1;
 		}
 		else
 		{
@@ -22,21 +27,49 @@ int coin(int money,int n,int bills[])
 	t= money / bills[n - 1];
 
 	for (int i = 0; i <= t; i++)
-	{
+	{		
 		cnt += coin(money - bills[n - 1] * i, n - 1, bills);
+		memo[n][money] = cnt;
 	}
 	return cnt;
+}
+
+void d(int money, int num[],int n)
+{
+	dp[0] = 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = num[i]; j <= money; j++)
+		{			
+			dp[j] += dp[j - num[i]];
+			if (dp[j] > 2000000000 && i == n )
+			{
+				printf("!!! %d %d %d\n",i,j,dp[j]);
+			}
+		}
+	}
+	printf("%d", dp[money]);
 }
 int main()
 {
 	int n, k;
-	int num[100] = { 0, };
-	scanf("%d %d", &n, &k);
+	int num[101] = { 0, };	
 	
-	for(int i=0;i<n;i++)
+	scanf("%d %d", &n, &k);
+
+	/*for (int i = 1; i <= n; i++)
 	{
 		scanf("%d", &num[i]);
 	}
+	
+	d(k,num,n);*/
 
-	printf("%d",coin(k, n, num));
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &num[i]);
+	}
+	printf("%d\n", coin(k, n, num));
 }
+
+
