30766ab1bed02e755df23c752103a3d714c53df2
KimDongUn <dong7061@gmail.com>
2018-02-26 13:14:11 +0900

    열개씩 끊어 출력
    2개방법

--- 11721.c
+++ 11721.c
@@ -1,18 +1,30 @@
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char s[101];
+//
+//	scanf("%s", s);
+//	for (int i = 0; i < strlen(s); i++)
+//	{		
+//		if (i!= 0 && i % 10 == 0)
+//		{
+//			printf("\n");
+//		}
+//		printf("%c", s[i]);
+//		
+//	}
+//}
+//-------------------------------------------------------
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-	char s[101];
+	char c[111];
 
-	scanf("%s", s);
-	for (int i = 0; i < strlen(s); i++)
-	{		
-		if (i!= 0 && i % 10 == 0)
-		{
-			printf("\n");
-		}
-		printf("%c", s[i]);
-		
+	while (scanf("%10s", c) == 1)
+	{
+		printf("%s\n", c);
 	}
 }
