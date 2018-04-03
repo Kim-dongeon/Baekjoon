9596fc44d6c3448f3bf36e73424b804378d397b7
KimDongUn <dong7061@gmail.com>
2018-04-03 17:42:42 +0900

    다음 순열

---
+++ 10972.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector<int> v(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> v[i];
+	}
+
+	if (next_permutation(v.begin(), v.end()))
+	{
+		for (int x : v)
+		{
+			cout << x << ' ';
+		}
+	}
+	else
+	{
+		printf("-1");
+	}
+}
